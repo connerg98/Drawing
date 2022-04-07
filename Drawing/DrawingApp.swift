b282aa8037f954ff55e59a19943dd8ac3ea926ba
Conner Glasgow <connerg38@gmail.com>
2022-04-06 23:24:34 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Conner Glasgow on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
